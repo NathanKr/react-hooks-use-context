44d02cbcaf449960797b57847432f64d1b38cd49
Nathan Krasney <natankrasney@gmail.com>
2021-12-15 20:29:32 +0200

    remove a warning

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Level1 from "./components/Level1";
 import { LanguageContext } from "./context/LanguageContext";
-import './App.css'
+import "./App.css";
 
 const App = () => {
   const [language, setLanguage] = useState("english");
@@ -11,11 +11,12 @@ const App = () => {
     <div className="App">
       <h2>App</h2>
       <h4>choose a language (by click) and notice the button text changes</h4>
-      <ui>
-      <li onClick={() => setLanguage("english")}>us</li>
-      <li onClick={() => setLanguage("portuguese")}>br</li>
-      </ui>
-      <br /><br />
+      <ul>
+        <li onClick={() => setLanguage("english")}>us</li>
+        <li onClick={() => setLanguage("portuguese")}>br</li>
+      </ul>
+      <br />
+      <br />
       <LanguageContext.Provider value={langObj}>
         <Level1 />
       </LanguageContext.Provider>
