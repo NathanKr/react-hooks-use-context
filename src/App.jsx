95ec7f6b6acc9e61b6b581a50629f9cd53d00b58
Nathan Krasney <natankrasney@gmail.com>
2021-12-15 11:22:13 +0200

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import Level1 from "./components/Level1";
 import { LanguageContext } from "./context/LanguageContext";
+import './App.css'
 
 const App = () => {
   const [language, setLanguage] = useState("english");
   const langObj = { language };
 
   return (
-    <div style={{ margin: "10px" }}>
+    <div className="App">
+      <h2>App</h2>
       <h4>choose a language (by click) and notice the button text changes</h4>
       <ui>
       <li onClick={() => setLanguage("english")}>us</li>
